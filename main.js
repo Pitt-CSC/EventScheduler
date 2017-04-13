ddeec7b8d737f63c825312c545ba3debc2b70bd8
John Linahan <linahan.john@gmail.com>
2017-04-13 14:25:06 -0400

    Insert data into dictionary and format time to minutes

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 (function (window, undefined) {
     "use strict";
 
+    var host = "http://localhost:5000/";
+
     function fetch_classes(term, code)
     {
-        var host = "http://localhost:5000/";
-
-        var num = 0;
 
         console.log("Fetching courses...");
         fetch(host + "courses/" + term + "/" + code)
@@ -14,8 +13,8 @@
                 return response.json();
             })
             .then(json => {
-                var classes = json.map((course) => {return course.class_number});
-                var num_classes = classes.length;
+                let classes = json.map((course) => {return course.class_number});
+                let num_classes = classes.length;
                 console.log("Fetching individual class details...");
                 Promise.all(classes.map((class_number) => {
                     return fetch(host + "class/" + class_number + "/" + term)
@@ -35,9 +34,39 @@
     }
 
 
-    //fetch_classes(2174, "cs");
+    //var CLASS_DATA = fetch_classes(2174, "cs");
+    var CLASS_DATA = mock;
 
-    console.log(mock);
+    function format_time(time)
+    {
+        let time_minutes = (parseInt(time.substring(0,2)) - 1) * 60 + parseInt(time.substring(3,4));
+
+        if(time.substring(6) == "PM" && time.substring(0,2) != "12")
+        {
+            time_minutes += 720;
+        }
+
+        return time_minutes;
+    }
+
+    var CLASS_TABLE = {};
+    CLASS_DATA.filter((cl) => {
+        if(cl.time[0] == "")
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }).forEach((cl) => {
+        cl.time[0] = format_time(cl.time[0]);
+        cl.time[1] = format_time(cl.time[1]);
+
+        CLASS_TABLE[cl.class_number] = cl;
+    });
+
+    console.log(CLASS_TABLE);
 
 
 })(typeof window !== "undefined" ? window : {});
